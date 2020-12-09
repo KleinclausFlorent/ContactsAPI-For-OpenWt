d6e33e07056dceee0a1da222391e42995f0ef6ec
Kleinclaus <kleinclaus.florent@gmail.com>
2020-12-09 17:46:03 +0100

    Authentification in web client is working. Todo : Authorization and checks for skills changes. Comment a bit

---
+++ MyContactsMVC/ViewModel/ErroViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyContactsMVC.ViewModel
+{
+    public class ErroViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
